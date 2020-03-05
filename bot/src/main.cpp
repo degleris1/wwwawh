7108253eaec653db01c42a31219958b596596634
degleris1 <degleris1@gmail.com>
2020-03-05 14:14:14 -0800

    Everything but tape sensing added

--- bot/src/main.cpp
+++ bot/src/main.cpp
@@ -1,8 +1,33 @@
 #include <Arduino.h>
 #include <Metro.h>
 
+
+
+// TODO
+// - Integrate tape sensor state
+
+
+
+// Event: right tape sensor triggered
+  // if (analogRead(PIN_LINE_RIGHT) < lineThresh) {
+  //   Serial.println("Right tape sensor trigger. Reverse counterclockwise.");
+  // }
+
+
+
+// --- Enumerated types ---
+enum State {
+  FINDING_BEACON,
+  ATTACKING_LEFT,
+  REVERSING,
+  ROTATING,
+  ATTACKING_RIGHT,
+  STOPPED
+};
+
+
 // --- Constants ---
-// - Pins
+// Pins
 const int PIN_LED = 13;
 
 const int PIN_LINE_LEFT = 14;
@@ -18,33 +43,34 @@ const int PIN_MOTOR_RIGHT_FWD = 1;
 const int PIN_MOTOR_RIGHT_REV = 3;
 const int PIN_MOTOR_RIGHT_PWM = 4;
 
-// - Intervals and frequencies
+// Intervals and frequencies
 const int INTERVAL_DEBUG = 1000000;  // 4 Hz
 const int INTERVAL_SAMPLE = 200;  // 5 KHz
 const int FREQ_PWM = 450;
 
-// - IR sensor
+// IR sensor
 const float GAIN = 0.888;
 const float IR_THRESH = 10.0;
 const float INTEGRAL_GAIN = 0.99;
 
-
+// Tape sensor
 const int TAPE_NUM_BASELINES = 10;
 const int TAPE_DELAY_BASELINE = 10;  // 10 ms
 const int TAPE_REL_THRESH = 0.5;  // 50 %
 
-// TODO all caps
-enum State {
-  findingBeacon,
-  attackingLeftWall,
-  reversing,
-  rotating,
-  attackingRightWall,
-  stopped
-};
+// Motor
+const int MOTOR_POWER_FULL = 1023;
+const int MOTOR_POWER_HALF = 500;
+const int MOTOR_POWER_ROTATE = 800;
+
+// Debug mode --- set to false for competitions and demonstrations
+const int DEBUG_MODE = false;
 
 
 // --- Prototypes ---
+void setupMotors(void);
+void setupSensors(void);
+
 void printDebug(void);
 void sampleSensors(void);
 
@@ -56,12 +82,12 @@ void rotateClockwiseR(void);
 
 
 // --- Module variables ---
-// - Timers
+// Timers
 IntervalTimer debugTimer;  // Timer for printing out debug information
-IntervalTimer sampleTimer;
+IntervalTimer sampleTimer;  // Timer for IR sensor sampling
 Metro stateMetro = Metro(100);
 
-// - Sensors
+// Sensors
 float lineThresh = 0;  // Threshold for line sensor (tape detection)
 float curIRSum = 0;
 float lastIRSum = 0;
@@ -73,44 +99,61 @@ float curIRIn = 0;
 float lastIRIn = 0;
 
 
-// - States
-State state = findingBeacon;
+// States
+State state = FINDING_BEACON;
 
 
 /**
  * Initialize Teensy.
  */
 void setup() {
-  // Initialize serial
-  Serial.begin(9600);
-  // while(!Serial);
-  Serial.println("Serial initialized.");
-
+  if (DEBUG_MODE) {
+    Serial.begin(9600);
+    Serial.println("Serial initialized.");
+  }
+  
   // Initialize pins
   analogReadResolution(10);
   analogWriteResolution(10);
 
-  // - LED on
+  // Activate Teensy LED
   pinMode(PIN_LED, OUTPUT);
   digitalWrite(PIN_LED, HIGH);
 
-  // - Input, line sensor
+  // Set up sensors and motors
+  setupSensors();
+  setupMotors();
+
+  // Set up timers
+  if (DEBUG_MODE) debugTimer.begin(printDebug, INTERVAL_DEBUG);  // Debug timer
+  sampleTimer.begin(sampleSensors, INTERVAL_SAMPLE);  // Sample timer
+
+  // Leave time for the switch operator to move away from the bot
+  delay(1000);  
+}
+
+
+void setupSensors() {
+  // Line sensor
   pinMode(PIN_LINE_LEFT, INPUT);
   pinMode(PIN_LINE_RIGHT, INPUT);
   for (int i = 0; i < TAPE_NUM_BASELINES; i++) {
     delay(TAPE_DELAY_BASELINE);
     lineThresh += analogRead(PIN_LINE_LEFT);
   }
-
-  //Serial.println(lineThresh);  // TODO debug this
   lineThresh = (lineThresh / TAPE_NUM_BASELINES) / 2;
-  //Serial.print("Line threshold: ");
-  //Serial.println(lineThresh);
 
-  // - Input, IR sensor
+  if (DEBUG_MODE) {
+    Serial.print("Line threshold: ");
+    Serial.println(lineThresh);
+  }
+
+  // IR sensor
   pinMode(PIN_IR_IN, INPUT);
+}
 
-  // - Motors
+
+void setupMotors() {
   pinMode(PIN_MOTOR_LEFT_FWD, OUTPUT);
   pinMode(PIN_MOTOR_LEFT_REV, OUTPUT);
   pinMode(PIN_MOTOR_LEFT_PWM, OUTPUT);
@@ -122,12 +165,6 @@ void setup() {
   analogWriteFrequency(PIN_MOTOR_RIGHT_PWM, FREQ_PWM);
 
   rotateClockwiseR();
-
-  // Set up timers
-  debugTimer.begin(printDebug, INTERVAL_DEBUG);  // Debug timer
-  sampleTimer.begin(sampleSensors, INTERVAL_SAMPLE);  // Sample timer
-
-  delay(1000);
 }
 
 
@@ -137,47 +174,41 @@ void setup() {
 void loop() {
   // Event: IR sensor triggered from findingBeacon state
   // Service: Move to drive forward state (with left motor overpowered)
-  if ((state == findingBeacon) && (curIRSum > 35.0)) {
+  if ((state == FINDING_BEACON) && (curIRSum > 35.0)) {
     driveForwardL();
-    state = attackingLeftWall;
+    state = ATTACKING_LEFT;
     stateMetro.interval(7.5 * 1000);
     stateMetro.reset();
   }
 
   // Event: timer expired from attackingLeftWall state
   // Service: Move to reverse state
-  if ((state == attackingLeftWall) && stateMetro.check()) {
+  if ((state == ATTACKING_LEFT) && stateMetro.check()) {
     driveReverseL();
-    state = reversing;
+    state = REVERSING;
     stateMetro.interval(2 * 1000);
     stateMetro.reset();
   }
 
-  if ((state == reversing) && stateMetro.check()) {
+  if ((state == REVERSING) && stateMetro.check()) {
     rotateClockwiseR();
-    state = rotating;
+    state = ROTATING;
     stateMetro.interval(2.5 * 1000);
     stateMetro.reset();
   }
 
-  if ((state == rotating) && stateMetro.check()) {
+  if ((state == ROTATING) && stateMetro.check()) {
     driveForwardR();
-    state = attackingRightWall;
+    state = ATTACKING_RIGHT;
     stateMetro.interval(15 * 1000);
     stateMetro.reset();
   }
 
-  if ((state == attackingRightWall) && stateMetro.check()) {
+  if ((state == ATTACKING_RIGHT) && stateMetro.check()) {
     stopMotors();
-    state = stopped;
-    // Et fini
+    state = STOPPED;
+    // Et fini.
   }
-  
-
-  // Event: right tape sensor triggered
-  // if (analogRead(PIN_LINE_RIGHT) < lineThresh) {
-  //   Serial.println("Right tape sensor trigger. Reverse counterclockwise.");
-  // }
 }
 
 
@@ -203,24 +234,24 @@ void driveForwardL() {
   // Left wheel -- forward
   digitalWrite(PIN_MOTOR_LEFT_FWD, HIGH);
   digitalWrite(PIN_MOTOR_LEFT_REV, LOW);
-  analogWrite(PIN_MOTOR_LEFT_PWM, 1023);
+  analogWrite(PIN_MOTOR_LEFT_PWM, MOTOR_POWER_FULL);
 
   // Right wheel -- forward, half
   digitalWrite(PIN_MOTOR_RIGHT_FWD, HIGH);
   digitalWrite(PIN_MOTOR_RIGHT_REV, LOW);
-  analogWrite(PIN_MOTOR_RIGHT_PWM, 500);
+  analogWrite(PIN_MOTOR_RIGHT_PWM, MOTOR_POWER_HALF);
 }
 
 void driveForwardR() {
   // Left wheel -- forward, half
   digitalWrite(PIN_MOTOR_LEFT_FWD, HIGH);
   digitalWrite(PIN_MOTOR_LEFT_REV, LOW);
-  analogWrite(PIN_MOTOR_LEFT_PWM, 1000);
+  analogWrite(PIN_MOTOR_LEFT_PWM, MOTOR_POWER_FULL);
 
   // Right wheel -- forward
   digitalWrite(PIN_MOTOR_RIGHT_FWD, HIGH);
   digitalWrite(PIN_MOTOR_RIGHT_REV, LOW);
-  analogWrite(PIN_MOTOR_RIGHT_PWM, 1023);
+  analogWrite(PIN_MOTOR_RIGHT_PWM, MOTOR_POWER_FULL);
 }
 
 
@@ -228,12 +259,12 @@ void driveReverseL() {
   // Left wheel -- reverse
   digitalWrite(PIN_MOTOR_LEFT_FWD, LOW);
   digitalWrite(PIN_MOTOR_LEFT_REV, HIGH);
-  analogWrite(PIN_MOTOR_LEFT_PWM, 1023);
+  analogWrite(PIN_MOTOR_LEFT_PWM, MOTOR_POWER_FULL);
 
   // Right wheel -- reverse, half
   digitalWrite(PIN_MOTOR_RIGHT_FWD, LOW);
   digitalWrite(PIN_MOTOR_RIGHT_REV, HIGH);
-  analogWrite(PIN_MOTOR_RIGHT_PWM, 500);
+  analogWrite(PIN_MOTOR_RIGHT_PWM, MOTOR_POWER_HALF);
 }
 
 
@@ -241,12 +272,12 @@ void rotateClockwiseR() {
   // Left wheel - forwards
   digitalWrite(PIN_MOTOR_LEFT_FWD, HIGH);
   digitalWrite(PIN_MOTOR_LEFT_REV, LOW);
-  analogWrite(PIN_MOTOR_LEFT_PWM, 800);
+  analogWrite(PIN_MOTOR_LEFT_PWM, MOTOR_POWER_ROTATE);
 
   // Right wheel - backwards
   digitalWrite(PIN_MOTOR_RIGHT_FWD, LOW);
   digitalWrite(PIN_MOTOR_RIGHT_REV, HIGH);
-  analogWrite(PIN_MOTOR_RIGHT_PWM, 800);
+  analogWrite(PIN_MOTOR_RIGHT_PWM, MOTOR_POWER_ROTATE);
 }
 
 
@@ -270,6 +301,8 @@ void printDebug() {
   Serial.println(curIRSum);
 
   // Print tape sensor output
-  // Serial.print("Left tape sensor: ");
-  // Serial.println(analogRead(PIN_LINE_LEFT));
+  Serial.print("Left tape sensor: ");
+  Serial.println(analogRead(PIN_LINE_LEFT));
+  Serial.print("Right tape sensor: ");
+  Serial.println(analogRead(PIN_LINE_RIGHT));
 }
